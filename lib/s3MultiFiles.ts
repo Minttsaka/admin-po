4874d9216ca18f2618e0dbf2bbcf7ec7d398f8fa
miracle <minttsaka@gmail.com>
2024-07-19 12:19:27 +0200

    changed next

--- lib/s3MultiFiles.ts
+++ lib/s3MultiFiles.ts
@@ -4,12 +4,12 @@ export async function MultiFiles(files:any){
     try{
         AWS.config.update({
             accessKeyId:process.env.NEXT_S3_ACCESS_KEY_ID,
-            secretAccessKey:process.env.NEXT_S3__SECRET_ACCESS_KEY,
+            secretAccessKey:process.env.NEXT_S3_SECRET_ACCESS_KEY,
         })
 
         const s3=new AWS.S3({
             params:{
-                Bucket:process.env.NEXT_S3__BUCKET_NAME,
+                Bucket:process.env.NEXT_S3_BUCKET_NAME,
             },
             region:'af-south-1'
         })
