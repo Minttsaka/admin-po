9553654d85e7a0c899da9deba5df4d63e4e296b8
miracle <minttsaka@gmail.com>
2024-07-19 11:42:52 +0200

    remove public

--- lib/s3.ts
+++ lib/s3.ts
@@ -3,13 +3,13 @@ import AWS from 'aws-sdk'
 export async function uploadToS3(file:File){
     try{
         AWS.config.update({
-            accessKeyId:process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
-            secretAccessKey:process.env.NEXT_PUBLIC_S3__SECRET_ACCESS_KEY,
+            accessKeyId:process.env.NEXT_S3_ACCESS_KEY_ID,
+            secretAccessKey:process.env.NEXT_S3__SECRET_ACCESS_KEY,
         })
 
         const s3=new AWS.S3({
             params:{
-                Bucket:process.env.NEXT_PUBLIC_S3__BUCKET_NAME,
+                Bucket:process.env.NEXT_S3__BUCKET_NAME,
             },
             region:'af-south-1'
         })
@@ -17,7 +17,7 @@ export async function uploadToS3(file:File){
         const fileKey='uploads/'+Date.now().toString()+file.name.replace(' ','-')
 
         const params={
-            Bucket:process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+            Bucket:process.env.NEXT_S3_BUCKET_NAME!,
             Key:fileKey,
             Body:file
         }
